a469f3f99762393a8cd6b18b26661c8b4288b0f8
Drew Pham <drewvpham@gmail.com>
2017-05-12 16:28:28 -0500

    solution

--- server.py
+++ server.py
@@ -4,19 +4,34 @@ from flask import Flask, render_template, redirect, request, session, flash
 import re
 # create a regular expression object that we can use run operations on
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+NOMBERS = re.compile(r'[0-9]')
 app = Flask(__name__)
 app.secret_key = "ThisIsSecret!"
 @app.route('/', methods=['GET'])
 def index():
+
   return render_template("index.html")
-  
+
 @app.route('/process', methods=['POST'])
 def submit():
-    if len(request.form['name'])< 1:
-        flash("Name cannot be blank!")
-    elif len(request.form['comments'])<1:
-        flash("Comments cant be blank!")
-    else:
-        flash("Success!")
+    session['email']=request.form['email']
+    session['first_name']=request.form['first_name']
+    session['last_name']=request.form['last_name']
+    session['password']=request.form['password']
+    session['confirm_password']=request.form['confirm_password']
+    message=[]
+    if len(session['email'])==0 or len(session['first_name'])==0 or len(session['last_name'])==0 or len(session['password'])==0 or len(session['confirm_password'])==0:
+        message.append("All fields are required and must not be blank!")
+        print len(session['email']), len(session['first_name']), len(session['last_name']), len(session['password']), len(session['confirm_password'])
+    if not EMAIL_REGEX.match(session['email']):
+        message.append("Invalid Email Address!")
+    if len(session['password'])<8:
+        message.append("Password should be more than 8 characters!")
+    if session['password'] is not session['confirm_password']:
+        message.append("Password and Password Confirmation should match")
+    if NOMBERS.match(session['first_name']) or NOMBERS.match(session['last_name']):
+        message.append("Why the fuck is there a number in your name?")
+    flash(message)
+
     return redirect('/')
 app.run(debug=True)
